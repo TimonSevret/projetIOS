73d93cf862ca88d2fdab9f109f91d8f0bb9ee283
tp <tp@tps-iMac.local>
2019-03-08 12:15:16 +0100

    commit debut

--- projetAppliAvancer/ViewController.swift
+++ projetAppliAvancer/ViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
+//gere la premiere vue, celle qui offre la meteo ect
 class ViewController: UIViewController {
 
+    @IBOutlet weak var humiditiLabel: UIBarButtonItem!
+    @IBOutlet weak var UVLabel: UIBarButtonItem!
+    @IBOutlet weak var DirVentLabel: UIBarButtonItem!
+    @IBOutlet weak var VitVentLabel: UIBarButtonItem!
+    @IBOutlet weak var TemperatureLabel: UILabel!
+    @IBOutlet weak var TempImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        //test lul
+        
         
     }
 
