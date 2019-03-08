8926ad56501a9aa6a65f74b467997d7e5667689d
tp <tp@tps-iMac.local>
2019-03-08 16:54:14 +0100

    commit à refactor

---
+++ projetAppliAvancer/City.swift
@@ -0,0 +1,16 @@
+//
+//  City.swift
+//  projetAppliAvancer
+//
+//  Created by tp on 08/03/2019.
+//  Copyright © 2019 tpxcode. All rights reserved.
+//
+
+import Foundation
+
+class City: Encodable, Decodable {
+    var titre: String = ""
+    var desc: String = ""
+    var valide: Bool = false
+
+}
---
+++ projetAppliAvancer/CityTableViewController.swift
@@ -0,0 +1,162 @@
+//
+//  CityTableViewController.swift
+//  projetAppliAvancer
+//
+//  Created by tp on 08/03/2019.
+//  Copyright © 2019 tpxcode. All rights reserved.
+//
+
+import UIKit
+
+class CityTableViewController: UITableViewController {
+ 
+    @IBOutlet weak var CityField: UITextField!
+    //juste import la view pour le clavier du textfield ?
+    
+    
+    var tableauCities = [City]()
+    var tableaucity2 = [City]()
+    
+    // cell reuse id (cells that scroll out of view can be reused)
+    let cellReuseIdentifier = "cell"
+    
+    let fichier = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Cities.plist")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Register the table view cell class and its reuse id
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        
+        // (optional) include this line if you want to remove the extra empty cell divider lines
+        // self.tableView.tableFooterView = UIView()
+        
+        // This view controller itself will provide the delegate methods and row data for the table view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        print(fichier) //in fine faire une classe spéciale pour les opérations de stockage
+ 
+        // Récupération des données depuis le fichier
+        do {
+            let recup = try Data(contentsOf: fichier!)
+            let decoder = PropertyListDecoder()
+            tableaucity2 = try decoder.decode([City].self, from: recup)
+        } catch {
+            print("Echec lecture depuis le fichier : \(error)")
+        }
+        
+        for city in tableaucity2 {
+            print(city.titre)
+        }
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationcity.rightBarButtoncity = self.editButtoncity
+    }
+
+    @IBAction func AddButton(_ sender: UIButton) {
+        do {
+            let recup = try Data(contentsOf: fichier!)
+            let decoder = PropertyListDecoder()
+            tableaucity2 = try decoder.decode([City].self, from: recup)
+        } catch {
+            print("Echec lecture depuis le fichier : \(error)")
+        }
+        let city=City(); city.titre=CityField.text!; city.valide=false; //a mettre en guard
+        tableaucity2.append(city)
+        
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(tableaucity2)
+            try data.write(to: fichier!)
+            print("ok")
+        } catch {
+            print("Echec écriture dans le fichier : \(error)")
+        }
+        self.tableView.reloadData()
+        //self.refresher ??
+    }
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return self.tableaucity2.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     // create a new cell if needed or reuse an old one
+     let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell!
+     
+     // set the text from the data model
+     cell.textLabel?.text = self.tableaucity2[indexPath.row].titre
+     
+     return cell
+        /*let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell*/
+    }
+ 
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified city to be editable.
+        return true
+    }
+    */
+
+    
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            tableaucity2.remove(at:indexPath.row)
+            let encoder = PropertyListEncoder()
+            do {
+                let data = try encoder.encode(tableaucity2)
+                try data.write(to: fichier!)
+            } catch {
+                print("Echec écriture dans le fichier : \(error)")
+            }
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the city to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
